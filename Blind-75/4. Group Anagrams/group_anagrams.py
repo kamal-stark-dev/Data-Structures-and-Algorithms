0297a348bc1ba3bd61c80a2890a0714ff2f82f4a
kamal-stark-dev <kamalveer356@gmail.com>
2026-07-24 23:02:40 +0530

    add group anagrams

---
+++ Blind-75/4. Group Anagrams/group_anagrams.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+
+class Solution:
+    """
+    """
+
+    def group_anagrams_sorting(self, strs: list[str]) -> list[list[str]]:
+        """
+        Time: O(m * n logn)
+        Space: O(m * n)
+
+        m = number of strings
+        n = length of the longest string
+        """
+
+        res = defaultdict(list)
+
+        for s in strs:
+            sorted_s = "".join(sorted(s))
+            res[sorted_s].append(s)
+
+        return list(res.values())
+
+
+    def group_anagrams_hash_set(self, strs: list[str]) -> list[list[str]]:
+        """
+        Time: O(m * n)
+        Space: O(m) auxilary space excluding the output, O(m * n) total space
+
+        m = number of strings
+        n = length of the longest string
+        """
+
+        res = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+
+            res[tuple(count)].append(s)
+
+        return list(res.values())
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    test_cases = [
+        (["eat","tea","tan","ate","nat","bat"], [["bat"],["nat","tan"],["ate","eat","tea"]]),
+        ([""], [[""]]),
+        (["a"], [["a"]])
+    ]
+
+    for strs, expected in test_cases:
+        result = solution.group_anagrams_hash_set(strs)
+
+        normalized_result = sorted([sorted(group) for group in result])
+        expected_result = sorted([sorted(group) for group in expected])
+
+        assert normalized_result == expected_result
+
+        print(
+            f"strs = {strs}\n"
+            f"got = {result}, expected = {expected}\n"
+        )
+
+    print("#######################\nAll test cases passed!!\n#######################")
